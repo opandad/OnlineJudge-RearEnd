0b4b6763ef5a4e018215083f66238b25c0558430
tSupan <t408805757@gmail.com>
2020-12-22 21:27:24 +0800

    修改数据库文件

--- database/OnlineJudgeMysql.sql
+++ database/OnlineJudgeMysql.sql
@@ -1,8 +1,7 @@
 /*
 功能：建立oj的mysql数据库
 日期：2020.12.16
-版本：0.0.2
-mysql版本：8.0.22
+版本：0.0.3
 */
 create database online_judge;
 use online_judge;
@@ -25,14 +24,9 @@ CREATE TABLE IF NOT EXISTS problems(
 	`fail` int unsigned,			-- 未通过数（计算百分比做数据统计用）
 	`judgeer_info` json, 			-- 负责储存判题地址，因为可能是爬虫下来的题目，存储格式为以下注释
 	/*
-						-- 先判定是否为爬虫，如为爬虫分两步走
-	is_robot_problem:true,
-	judgger:xxxOJ
+	-- 先判定是否为爬虫，如为爬虫分两步走
+	judgger:xxxOJ/local
 	oj_problem_id:1000
-
-
-	is_robot_problem:false,
-
 
 	language:{				-- 必定存储的语言及判题方式，或着直接爬
 		--sample
@@ -55,19 +49,18 @@ create table IF NOT EXISTS contest(
 	`id` int unsigned auto_increment,
 	`name` tinytext,
 	`start_time` datetime,
-	`end_time` datetime, 			-- 可能需要修改
-	`match_info` json,			-- 负责开关，以及比赛用户过滤，存储注释内的内容
+	`contest_length` time,
+	`contest_info` json,			-- 负责开关，以及比赛用户过滤，存储注释内的内容
+	`is_official_match` boolean,
 	/*
 	-- 判断两步走
 	-- true存储大量相关信息
-	is_official_match:true
-		password:
+		password:			-- 注意加密
 		user:[]				-- 存储比赛用户
 		organizer:			-- 举办方
 		sponsor:			-- 赞助方
 
 	--false不存储任何信息
-	is_official_match:false
 	*/
 	`problem_id` json,			-- 负责存储题目编号方便读取
 	primary key(`id`)
@@ -84,11 +77,4 @@ create table IF NOT EXISTS submit_status(
 	`user_email` char(30) not null,
 	`run_time` smallint unsigned,
 	primary key(`submit_id`)
-)ENGINE=INNODB;
-
-
-create table IF NOT EXISTS support_language(
-	`language` char(50),
-	`submit_command` text,
-	primary key(`language`)
 )ENGINE=INNODB;
